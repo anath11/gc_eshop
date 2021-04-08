1515dc777c79e68426d3920e19a389934e5c9e29
Ashok <ashoknath@Ashoks-MBP.hub>
2021-04-08 13:28:48 +1000

    Full stack Mern Application

---
+++ frontend/build/precache-manifest.27a85ae30b59a46bf9fdd18c5f9c9c1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a55b45bfc58be221ab4254da70256e59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fc2bbe5f5d447467df4",
+    "url": "/static/css/main.e47b45a8.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fc2bbe5f5d447467df4",
+    "url": "/static/js/main.1c09bfae.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
